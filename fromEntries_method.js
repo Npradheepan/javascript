b33a022e2f8ea6b52d6bb6c8bde104b522bdfd5e
Npradheepan <44838133+Npradheepan@users.noreply.github.com>
2021-07-09 12:46:53 +0200

    Create fromEntries_method.js

---
+++ fromEntries_method.js
@@ -0,0 +1,4 @@
+let map  = new Map ([[`name1`,`abc`],[`name2`,`pqr`], [`name`,`xyz`]]);
+           // console.log(map);
+           let object = Object.fromEntries(map);
+           console.log(object);
